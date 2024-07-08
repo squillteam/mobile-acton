f8bc785dc25c82cc8a3de49933594b289095c067
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-07 22:31:02 -0300

    cleanup: Remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:acton/base/base_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:esc_pos_printer/esc_pos_printer.dart';
-import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -18,88 +16,12 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Acton',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const BaseScreen(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-
-  final _errors = List<String>.empty(growable: true); 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 10),
-              child: const Text("Testar na impressora com IP: 10.0.6.2"),
-            ), 
-            GestureDetector(
-              child: Container(
-                width: 200,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Center(
-                  child: Text(
-                    "IMPRIMIR",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-              ),
-              onTap: () async {
-                
-              },
-            ),
-            ...() {
-              return _errors.map((e) => Text(e.toString(), textAlign: TextAlign.center,));
-            } ()
-          ]
-        ),
-      )// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
